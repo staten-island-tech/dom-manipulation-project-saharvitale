3275101bc83082f13b3f6319c75f3c42274f6594
saharvitale <113132357+saharvitale@users.noreply.github.com>
2023-01-03 13:59:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 //; const button = document.getElementById("btn");
 //; const text = document.querySelector("#text");
 //; console.log(text);
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  empty: document.querySelector(".nothing"),
+  //use "." for classes in querySelector
+  point: document.querySelectorAll(".point"),
+};
+console.log(DOMSelectors);
