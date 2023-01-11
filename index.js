ae8225413195895e3a6cb847106c98bc232b9ec1
saharvitale <113132357+saharvitale@users.noreply.github.com>
2023-01-11 13:59:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,15 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
+  //use "#" for ids in querySelector
   box: document.getElementById("big-black-box"),
   //use "." for classes in querySelector
   points: document.querySelectorAll(".point"),
 };
 
-console.log(DOMSelectors);
+function backgroundaTxt(background, text) {
+  background.style.backgroundColor = "red";
+  text.innerHTML = "this is a red box";
+}
+
+backgroundaTxt(DOMSelectors.box, DOMSelectors.text);
